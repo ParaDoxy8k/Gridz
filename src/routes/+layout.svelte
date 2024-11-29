01f133acb86924660cebb752b41b747581e3559d
Paralez <dragontty644@gmail.com>
2024-11-29 21:11:04 +0700

    modified:   package-lock.json 	new file:   project.inlang/project_id 	modified:   src/app.css 	modified:   src/component/Header.svelte 	renamed:    src/component/Footer.svelte -> src/component/Homepage.svelte 	modified:   src/component/Main.svelte 	new file:   src/component/Portfolio.svelte 	modified:   src/routes/+layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,10 @@
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	let { children } = $props();
-	import Footer from "../component/Footer.svelte";
-  	import Header from "../component/Header.svelte";
+
 </script>
-<Header />
-<Footer />
+
 <ParaglideJS {i18n}>
-	{@render children()}
+{@render children()}
 </ParaglideJS>
 
