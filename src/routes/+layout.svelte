2df017e2bcce21c0cd91ab9db8530326c6678598
Paralez <dragontty644@gmail.com>
2024-11-30 23:28:31 +0700

    modified:   src/component/Header.svelte 	modified:   src/component/Main.svelte 	modified:   src/routes/+layout.svelte 	new file:   static/image/people/profile.JPEG

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,9 @@
 	import { i18n } from '$lib/i18n';
 	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
 	let { children } = $props();
-
+	import Header from '../component/Header.svelte';
 </script>
-
+<Header />
 <ParaglideJS {i18n}>
 {@render children()}
 </ParaglideJS>
